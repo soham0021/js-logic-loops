c25790d93d7897252ae2f4721ab84b8f71369db4
soham patel <sohampatel9055@yahoo.com>
2016-12-08 18:56:20 -0500

    begining js-logic-loops

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// creat a variable with string stored inside
+var lol = 'soham';
+// creat a variable with a number stored inside
+var number = 3;
+// creat a variable with a boolen stored inside
+var killit = true;
+// creat a variable with an array stored inside
+var champtype =['mage','figher','suport','tanks','marksman'];
+// creat a variable with an object stored inside
+var info = {
+	kill: 10,
+	death: 2,
+	assaist:5
+	game:info
+};
+console.log(info);
+
+// accese an objects property using dot notation
+console.log(info.info);
+
+// access an iteam in a array using its index
+console.log(info.info[0]); // logs 'mage'
